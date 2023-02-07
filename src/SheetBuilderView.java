418ce4dd0c73598708da2f98523a291cdaff4b5c
Noah Lake (School) <noahlake07@gmail.com>
2023-02-07 09:07:11 -0700

    Initial Commit

---
+++ src/SheetBuilderView.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class SheetBuilderView extends JFrame {
+
+    public SheetBuilderView(){
+        // Frame setup
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,400);
+        setVisible(true);
+
+    }
+
+}
