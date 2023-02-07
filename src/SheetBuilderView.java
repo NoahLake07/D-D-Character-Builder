a9f6c28fe07d14e6ec5860c1b00b18a4d1e81e42
Noah Lake (School) <noahlake07@gmail.com>
2023-02-07 09:20:53 -0700

    Started Tabbed Panes

--- src/SheetBuilderView.java
+++ src/SheetBuilderView.java
@@ -1,11 +1,38 @@
+import com.prog2.panels.AbilityPanel;
+import com.prog2.panels.ClassPanel;
+import com.prog2.panels.InfoPanel;
+import com.prog2.panels.RacePanel;
+
 import javax.swing.*;
 
 public class SheetBuilderView extends JFrame {
 
+    private JTabbedPane tabs;
+    private JPanel raceTab, infoTab, abilityTab, classTab;
+
+
     public SheetBuilderView(){
+        // Setup tabbed pane
+        tabs = new JTabbedPane();
+
+        // setup
+        raceTab = new RacePanel();
+        classTab = new ClassPanel();
+        infoTab = new InfoPanel();
+        abilityTab = new AbilityPanel();
+
+        // add all panels to the tab pane
+        tabs.add("Choose Race", raceTab);
+        tabs.add("Choose Class", classTab);
+        tabs.add("Choose Name/Background",infoTab);
+        tabs.add("Roll Abilities", abilityTab);
+
+        add(tabs);
+
         // Frame setup
+        setTitle("Character Setup - D&D");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400,400);
+        setSize(700,400);
         setVisible(true);
 
     }
---
+++ src/com/prog2/panels/RacePanel.java
@@ -0,0 +1,15 @@
+package com.prog2.panels;
+
+import javax.swing.*;
+
+public class RacePanel extends JPanel {
+
+    JComboBox<String> raceCombo;
+
+    public RacePanel(){
+        super();
+
+        raceCombo = new JComboBox<>();
+    }
+
+}
